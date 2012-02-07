4450e337e2938b40660a3de883d16d6025670dd1
Roger <wu@unaskthequestion.net>
2012-02-08 00:12:29 +0100

    Stand-alone JAR

--- src/SolitudeClient.java
+++ src/SolitudeClient.java
@@ -14,6 +14,22 @@ import netP5.*;
 import java.awt.Color;
 import java.util.Vector;
 
+import processing.xml.*; 
+
+import java.applet.*; 
+import java.awt.Dimension; 
+import java.awt.Frame; 
+import java.awt.event.MouseEvent; 
+import java.awt.event.KeyEvent; 
+import java.awt.event.FocusEvent; 
+import java.awt.Image; 
+import java.io.*; 
+import java.net.*; 
+import java.text.*; 
+import java.util.*; 
+import java.util.zip.*; 
+import java.util.regex.*; 
+
 public class SolitudeClient extends PApplet {
 	
 	ControlP5 controlP5;
@@ -159,11 +175,12 @@ public class SolitudeClient extends PApplet {
 	
 
 
-	/*
+	
 	static public void main(String args[]) {
-	    PApplet.main(new String[] { "Solitude" });
-	  }
-	  */
+		PApplet.main(new String[] { "--bgcolor=#FFFFFF", "SolitudeClient" });
+	}
+	  
+	  
 	
 }
 
